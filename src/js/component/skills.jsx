49040dedd56e52eb33bbfd369ede6f0ef23b03b9
nestorfron <nestorfrones07@hotmail.com>
2024-06-23 23:25:39 -0300

    habilidades y trabajos2

--- src/js/component/skills.jsx
+++ src/js/component/skills.jsx
@@ -3,58 +3,49 @@ import img from '../../img/3lb.jpg'
 
 export const Skills = () => (
     <div className="text-center pb-3">
-        <div className="container m-auto justify-content-between shadow-lg">
+        <div className="container m-auto justify-content-between shadow p-5">
             <h1 className="py-5">Habilidades</h1>
-            <div className="flex-row card m-2 p-2">
+            <div className="flex-row card m-2 p-2 shadow-lg">
                 <div className="col-3">
                     <img src={img} className="card-img-top" alt="..." />
                 </div>
-                <div className="card-body col-3 m-auto justify-content-between">
+                <div className="card-body col-3 m-auto">
                     <h5 className="card-title">Card title</h5>
                     <p className="card-text">descripción breve</p>
                 </div>
             </div>
-            <div className="flex-row card m-2 p-2">
+            <div className="flex-row card m-2 p-2 shadow-lg">
                 <div className="col-3">
                     <img src={img} className="card-img-top" alt="..." />
                 </div>
-                <div className="card-body col-3 m-auto justify-content-between">
+                <div className="card-body col-3 m-auto">
                     <h5 className="card-title">Card title</h5>
                     <p className="card-text">descripción breve</p>
                 </div>
             </div>
-            <div className="flex-row card m-2 p-2">
+            <div className="flex-row card m-2 p-2 shadow-lg">
                 <div className="col-3">
                     <img src={img} className="card-img-top" alt="..." />
                 </div>
-                <div className="card-body col-3 m-auto justify-content-between">
+                <div className="card-body col-3 m-auto">
                     <h5 className="card-title">Card title</h5>
                     <p className="card-text">descripción breve</p>
                 </div>
             </div>
-            <div className="flex-row card m-2 p-2">
+            <div className="flex-row card m-2 p-2 shadow-lg">
                 <div className="col-3">
                     <img src={img} className="card-img-top" alt="..." />
                 </div>
-                <div className="card-body col-3 m-auto justify-content-between">
+                <div className="card-body col-3 m-auto">
                     <h5 className="card-title">Card title</h5>
                     <p className="card-text">descripción breve</p>
                 </div>
             </div>
-            <div className="dflex-row card m-2 p-2">
+            <div className="flex-row card m-2 p-2 shadow-lg">
                 <div className="col-3">
                     <img src={img} className="card-img-top" alt="..." />
                 </div>
-                <div className="card-body col-3 m-auto justify-content-between">
-                    <h5 className="card-title">Card title</h5>
-                    <p className="card-text">descripción breve</p>
-                </div>
-            </div>
-            <div className="flex-row card m-2 p-2">
-                <div className="col-3">
-                    <img src={img} className="card-img-top" alt="..." />
-                </div>
-                <div className="card-body col-3 m-auto justify-content-between">
+                <div className="card-body col-3 m-auto">
                     <h5 className="card-title">Card title</h5>
                     <p className="card-text">descripción breve</p>
                 </div>
