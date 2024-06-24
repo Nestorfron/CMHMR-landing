eee9b9418afd34452b96d593e1fd8ab9682a9d26
nestorfron <nestorfrones07@hotmail.com>
2024-06-23 22:48:02 -0300

    contact

--- src/js/component/contact.jsx
+++ src/js/component/contact.jsx
@@ -2,76 +2,28 @@ import React from "react"
 
 
 export const Contact = () => (
-
-                <div id='contact' className='w-full lg:h-screen'>
-                        <div className='max-w-[1240px] m-auto px-2 py-16 w-full'>
-                                <p className='text-xl tracking-widest uppercase text-[#4791C2]'>Contact</p>
-                                <h2 className='py-4'>Get In Touch</h2>
-                                <div className='grid lg:grid-cols-5 gap-8'>
-                                        <div className='col-span-3 lg:col-span-2 w-full h-full shadow-xl shadow-gray-400 rounded-xl p-4'>
-                                                <div className='lg:p-4 h-full'>
-                                                        <div>
-                                                                <Image className='rounded-xl hover:scale-105 ease-in duration-300' src={contactImg} alt='/' />
-                                                        </div>
-                                                        <div>
-                                                                <h2 className='py-2'>Nestor Frones</h2>
-                                                                <p>Front-End Developer</p>
-                                                                <p className='py-4'>I am available for freelance or full-time positions. Contact me and let´s talk.</p>
-                                                        </div>
-                                                        <div>
-                                                                <p className='uppercase pt-8'>Connect With Me</p>
-                                                                <div className='flex items-center justify-center py-4'>
-                                                                        <div className='rounded-full shadow-lg shadow-gray-400 ml-4 p-6 cursor-pointer hover:scale-110 ease-in duration-500'>
-                                                                                <a href="https://www.linkedin.com/in/nestor-frones/"><FaLinkedinIn /></a>
-                                                                        </div>
-                                                                        <div className='rounded-full shadow-lg shadow-gray-400 ml-4 p-6 cursor-pointer hover:scale-110 ease-in duration-500'>
-                                                                                <a href="https://github.com/Nestorfron"><FaGithub /></a>
-                                                                        </div>
-                                                                </div>
-                                                        </div>
-                                                </div>
-                                        </div>
-                                        <div className='col-span-3 w-full h-auto shadow-xl stroke-gray-400 rounded-xl lg:p-4'>
-                                                <div className='p-4'>
-                                                        <form action="https://formsubmit.co/nestorfrones07@gmail.com" method="POST" >
-                                                                <div className='grid md:grid-cols-2 gap-4 w-full py-2'>
-                                                                        <div className='flex flex-col'>
-                                                                                <label className='uppercase text-sm py-2'>Name</label>
-                                                                                <input className='border-2 rounded-lg p-3 flex border-gray-300' type="text" required name='Name' />
-                                                                        </div>
-                                                                        <div className='flex flex-col'>
-                                                                                <label className='uppercase text-sm py-2'>Phone Number</label>
-                                                                                <input className='border-2 rounded-lg p-3 flex border-gray-300' type="text" required name='Phone Number' />
-                                                                        </div>
-                                                                </div>
-                                                                <div className='flex flex-col py-2'>
-                                                                        <label className='uppercase text-sm py-2'>Email</label>
-                                                                        <input className='border-2 rounded-lg p-3 flex border-gray-300' type="email" required name='Email' />
-                                                                </div>
-                                                                <div className='flex flex-col py-2'>
-                                                                        <label className='uppercase text-sm py-2'>Subjet</label>
-                                                                        <input className='border-2 rounded-lg p-3 flex border-gray-300' type="text" name='Subjet' />
-                                                                </div>
-                                                                <div className='flex flex-col py-2'>
-                                                                        <label className='uppercase text-sm py-2'>Message</label>
-                                                                        <textarea className='border-2 rounded-lg p-3 border-gray-300' rows='10' name='Text area'></textarea>
-                                                                </div>
-                                                                <button className='w-full p-4 text-gray-100 mt-4'>Send Message</button>
-                                                                <input type="hidden" name="_captcha" value="false" />
-                                                                <input type="hidden" name="_next" value="https://portfoliov6.vercel.app/thanks"></input>
-                                                        </form>
-                                                </div>
-                                        </div>
-                                </div>
-                                <div className='flex justify-center p-12'>
-                                        <Link href='/'>
-                                                <div className='rounded-full shadow-lg shadow-gray-400 p-4 cursor-pointer hover:scale-110 ease-in duration-500'>
-                                                        <HiChevronDoubleUp className='text-[#4791C2]' size={30} />
-                                                </div>
-                                        </Link>
-                                </div>
+        <form>
+                <h1 className="my-5" >Contacto</h1>
+                <div className="container text-start ounded-3 pb-5 shadow-lg  p-4">
+                        <div className="mb-3">
+                                <label className="form-label">Nombre y Apellido</label>
+                                <input type="text" class="form-control" aria-label="Sizing example input" aria-describedby="inputGroup-sizing-default" />
                         </div>
+                        <div className="mb-3">
+                                <label className="form-label">Número de contacto</label>
+                                <input type="text" class="form-control" aria-label="Sizing example input" aria-describedby="inputGroup-sizing-default" />
+                        </div>
+                        <div className="mb-3">
+                                <label className="form-label">Email</label>
+                                <input type="email" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" />
+                        </div>
+                        <div class="mb-3">
+                                <label for="exampleFormControlTextarea1" class="form-label">Mensaje</label>
+                                <textarea class="form-control" id="exampleFormControlTextarea1" rows="3"></textarea>
+                        </div>
+                        <button type="submit" className="btn btn-primary mb-5">Enviar</button>
                 </div>
-        )
+        </form>
+)
 
 
--- src/js/component/footer.js
+++ src/js/component/footer.js
@@ -1,7 +1,7 @@
 import React, { Component } from "react";
 
 export const Footer = () => (
-	<footer className="footer m-auto p-3 text-center fixed-bottom shadow">
+	<footer className="footer m-auto p-3 text-center fixed-bottom shadow-lg">
 		<small>
 			(NF)3 management 2024
 		</small>
