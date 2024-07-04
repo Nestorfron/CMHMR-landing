3f8efde80cfa7b9543e82a94801739af59c1a33a
Nestor Frones <115091611+Nestorfron@users.noreply.github.com>
2024-07-04 14:13:51 +0000

    add footer

--- src/js/component/footer.js
+++ src/js/component/footer.js
@@ -1,9 +1,11 @@
-import React, { Component } from "react";
+import React from "react";
+import "../../styles/index.css"
+
 
 export const Footer = () => (
 	<footer className="footer m-auto p-3 text-center bg-dark">
 		<small>
-			(NF)3 management 2024
+			hola
 		</small>
 	</footer>
 );
