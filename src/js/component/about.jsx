e0e13f0ecfc8574c146ed2edfb163558c67911dd
nestorfron <nestorfrones07@hotmail.com>
2024-07-04 00:02:55 -0300

    skills

--- src/js/component/about.jsx
+++ src/js/component/about.jsx
@@ -1,7 +1,5 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faArrowUp } from '@fortawesome/free-solid-svg-icons'
 import "../../styles/index.css"
 import img from "../../img/logoBlanco.png"
 
@@ -9,24 +7,28 @@ import img from "../../img/logoBlanco.png"
 
 export const About = () => (
     <div id="about" className="text-center">
-        <div className="card-header container col-sm-12">
+        <div className="container-fluid px-5 col-sm-12">
             <img src={img} alt="Logo" width="70%" height="50%" className="image d-inline-block align-text-top" />
-            <p className="text-white">Desarrollador Full Stack con experiencia en el desarrollo de aplicaciones web y móviles, especializado en tecnologías front-end y back-end. Apasionado por crear soluciones eficientes, seguras y escalables, con un enfoque en la experiencia del usuario y la optimización del rendimiento.</p>
+            <p className="text-white fs-5">Desarrollador Full Stack con experiencia en el desarrollo de aplicaciones web y móviles, especializado en tecnologías front-end y back-end. Apasionado por crear soluciones eficientes, seguras y escalables, con un enfoque en la experiencia del usuario y la optimización del rendimiento.</p>
         </div>
-        <div className="card-body fs-2">
+        <div className="card-body">
             <Link>
-                <i className="fa-brands fa-facebook m-4  text-white"></i>
+                <i className="icon-about fa-brands fa-facebook m-4  text-white"></i>
             </Link>
             <Link>
-                <i className="fa-brands fa-linkedin m-4  text-white"></i>
+                <i className="icon-about fa-brands fa-linkedin m-4  text-white"></i>
             </Link>
             <Link>
-                <i className="fa-brands fa-github m-4  text-white"></i>
+                <i className="icon-about fa-brands fa-github m-4  text-white"></i>
             </Link>
             <Link>
-                <i className="fa-brands fa-whatsapp m-4  text-white"></i>
+                <i className="icon-about fa-brands fa-whatsapp m-4  text-white"></i>
             </Link>
-
+            <div id="div-totop" className="cmn-divfloat">
+                <a href="#" className="btn btn-dark cmn-btncircle">
+                    <i class="fa-solid fa-arrow-up"></i>
+                </a>
+            </div>
         </div>
     </div>
 );
--- src/js/component/skills.jsx
+++ src/js/component/skills.jsx
@@ -2,7 +2,37 @@ import React from "react";
 
 
 export const Skills = () => (
-    <div id="skills" className="fondo text-center pb-3">
-        hola
+    <div id="skills" className="container row text-center p-3 m-auto">
+        <div className="m-auto border-bottom mb-3">
+            <h1 className="skills-title p-3 m-3">Conocimientos</h1>
+        </div>
+        <div className="col p-4 mx-4 text-white">
+            <i className="my-auto icon-skill fa-brands fa-bootstrap"></i>
+            <p className="my-auto p-skill px-3 card-text">Boostrap</p>
+        </div>
+        <div className="col p-4 mx-4 text-white">
+            <i className="fa-brands icon-skill fa-html5"></i>
+            <p className="my-auto p-skill px-3 card-text">HTML5</p>
+        </div>
+        <div className="col p-4 mx-4 text-white">
+            <i className="fa-brands icon-skill fa-css3-alt"></i>
+            <p className="my-auto p-skill px-3 card-text">CSS</p>
+        </div>
+        <div className="col p-4 mx-4 text-white">
+            <i className="fa-brands icon-skill fa-react"></i>
+            <p className="my-auto p-skill px-3 card-text">React</p>
+        </div>
+        <div className="col p-4 mx-4 text-white">
+            <i className="fa-brands icon-skill fa-github"></i>
+            <p className="my-auto p-skill px-3 card-text">GitHub</p>
+        </div>
+        <div className="col p-4 mx-4 text-white">
+            <i className="fa-solid icon-skill fa-flask"></i>
+            <p className="my-auto p-skill px-3 card-text">Flask</p>
+        </div>
+        <div className="col p-4 mx-4 text-white">
+            <i className="fa-solid icon-skill fa-database"></i>
+            <p className="my-auto p-skill px-3 card-text">MSql</p>
+        </div>
     </div>
 );
