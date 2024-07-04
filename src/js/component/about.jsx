221d6bb8cfe8a6b17af967cb67e42fdaff0427c0
nestorfron <nestorfrones07@hotmail.com>
2024-07-04 20:53:31 -0300

    changes

--- src/js/component/about.jsx
+++ src/js/component/about.jsx
@@ -2,11 +2,13 @@ import React from "react";
 import { Link } from "react-router-dom";
 import "../../styles/index.css"
 import img from "../../img/logoBlanco.png"
+import img0 from "../../img/0.jpg"
 
 
 
 export const About = () => (
-    <div id="about" className="text-center">
+    <div id="about" style={{ backgroundImage: `url(${img0})`, backgroundSize: 'cover', backgroundPosition: 'center', zIndex1: -1 }} className="text-center">
+        <div className="about-container container-fluid" >
         <div className="container-fluid px-5 col-sm-12">
             <img src={img} alt="Logo" width="70%" height="50%" className="image d-inline-block align-text-top" />
             <p className="text-white fs-5">Desarrollador Full Stack con experiencia en el desarrollo de aplicaciones web y móviles, especializado en tecnologías front-end y back-end. Apasionado por crear soluciones eficientes, seguras y escalables, con un enfoque en la experiencia del usuario y la optimización del rendimiento.</p>
@@ -30,6 +32,7 @@ export const About = () => (
                     <i class="fa-solid fa-arrow-up"></i>
                 </a>
             </div>
+        </div>
         </div>
     </div>
 );
