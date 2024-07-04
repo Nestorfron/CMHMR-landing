bdbd7d12029f193c72f9644eef3a5d9c156b0cc2
Nestor Frones <115091611+Nestorfron@users.noreply.github.com>
2024-07-04 11:12:21 +0000

    add projcts

--- src/js/component/projects.jsx
+++ src/js/component/projects.jsx
@@ -1,70 +1,72 @@
 import React from "react";
+import img from "../../img/logo.png"
+
 
 
 export const Projects = () => (
     <div id="projects" className="fondo text-center pb-3">
         <div className="container row m-auto justify-content-center">
             <div className="project-card card col-8 col-md-4 col-sm-4 m-4">
-                <img src="..." className="card-img-top" alt="..." />
+                <img src={img} className="project-img card-img-top" alt="..." />
                 <div className="card-body">
                     <h5 className="card-title">Tienda Virtual</h5>
                     <p className="card-text">HTML-CSS-React</p>
-                    <a href="#" className="btn btn-primary">Demo</a>
-                    <a href="#" className="btn btn-primary">Code</a>
+                    <a href="#" className="btn btn-primary m-1">Demo</a>
+                    <a href="#" className="btn btn-primary m-1">Code</a>
                 </div>
             </div>
             <div className="project-card card col-8 col-md-4 col-sm-4 m-4">
-                <img src="..." className="card-img-top" alt="..." />
+                <img src={img} className="project-img card-img-top" alt="..." />
                 <div className="card-body">
                     <h5 className="card-title">Tienda Virtual</h5>
                     <p className="card-text">HTML-CSS-React</p>
-                    <a href="#" className="btn btn-primary">Demo</a>
-                    <a href="#" className="btn btn-primary">Code</a>
+                    <a href="#" className="btn btn-primary m-1">Demo</a>
+                    <a href="#" className="btn btn-primary m-1">Code</a>
                 </div>
             </div>
             <div className="project-card card col-8 col-md-4 col-sm-4 m-4">
-                <img src="..." className="card-img-top" alt="..." />
+                <img src={img} className="project-img card-img-top" alt="..." />
                 <div className="card-body">
                     <h5 className="card-title">Tienda Virtual</h5>
                     <p className="card-text">HTML-CSS-React</p>
-                    <a href="#" className="btn btn-primary">Demo</a>
-                    <a href="#" className="btn btn-primary">Code</a>
+                    <a href="#" className="btn btn-primary m-1">Demo</a>
+                    <a href="#" className="btn btn-primary m-1">Code</a>
                 </div>
             </div>
             <div className="project-card card col-8 col-md-4 col-sm-4 m-4">
-                <img src="..." className="card-img-top" alt="..." />
+                <img src={img} className="project-img card-img-top" alt="..." />
                 <div className="card-body">
                     <h5 className="card-title">Tienda Virtual</h5>
                     <p className="card-text">HTML-CSS-React</p>
-                    <a href="#" className="btn btn-primary">Demo</a>
-                    <a href="#" className="btn btn-primary">Code</a>
+                    <a href="#" className="btn btn-primary m-1">Demo</a>
+                    <a href="#" className="btn btn-primary m-1">Code</a>
                 </div>
             </div>
             <div className="project-card card col-8 col-md-4 col-sm-4 m-4">
-                <img src="..." className="card-img-top" alt="..." />
+                <img src={img} className="project-img card-img-top" alt="..." />
                 <div className="card-body">
                     <h5 className="card-title">Tienda Virtual</h5>
                     <p className="card-text">HTML-CSS-React</p>
-                    <a href="#" className="btn btn-primary">Demo</a>
-                    <a href="#" className="btn btn-primary">Code</a>
+                    <a href="#" className="btn btn-primary m-1">Demo</a>
+                    <a href="#" className="btn btn-primary m-1">Code</a>
                 </div>
             </div>
             <div className="project-card card col-8 col-md-4 col-sm-4 m-4">
-                <img src="..." className="card-img-top" alt="..." />
+                <img src={img} className="project-img card-img-top" alt="..." />
                 <div className="card-body">
                     <h5 className="card-title">Tienda Virtual</h5>
                     <p className="card-text">HTML-CSS-React</p>
-                    <a href="#" className="btn btn-primary">Demo</a>
-                    <a href="#" className="btn btn-primary">Code</a>
+                    <a href="#" className="btn btn-primary m-1">Demo</a>
+                    <a href="#" className="btn btn-primary m-1">Code</a>
                 </div>
             </div>
             <div className="project-card card col-8 col-md-4 col-sm-4 m-4">
-                <img src="..." className="card-img-top" alt="..." />
+                <img src={img} className="project-img card-img-top" alt="..." />
                 <div className="card-body">
                     <h5 className="card-title">Tienda Virtual</h5>
                     <p className="card-text">HTML-CSS-React</p>
-                    <a href="#" className="btn btn-primary">Demo</a>
-                    <a href="#" className="btn btn-primary">Code</a>
+                    <a href="#" className="btn btn-primary m-1">Demo</a>
+                    <a href="#" className="btn btn-primary m-1">Code</a>
                 </div>
             </div>
         </div>
