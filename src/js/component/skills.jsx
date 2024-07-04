4d467077792dd3ed4ff91e0e83ac147a929ca110
Nestor Frones <115091611+Nestorfron@users.noreply.github.com>
2024-07-04 10:16:55 +0000

    fix promlem 2

--- src/js/component/skills.jsx
+++ src/js/component/skills.jsx
@@ -7,31 +7,31 @@ export const Skills = () => (
             <h1 className="skills-title p-3 m-3">Conocimientos</h1>
         </div>
         <div className="row">
-            <div className="col-4 col-sm-12 p-4 mx-4 text-white">
+            <div className="col col-sm-4 p-4 mx-4 text-white">
                 <i className="fa-brands icon-skill fa-html5"></i>
                 <p className="my-auto p-skill px-3 card-text">HTML5</p>
             </div>
-            <div className="col-4 col-sm-12 p-4 mx-4 text-white">
+            <div className="col col-sm-4 p-4 mx-4 text-white">
                 <i className="fa-brands icon-skill fa-css3-alt"></i>
                 <p className="my-auto p-skill px-3 card-text">CSS</p>
             </div>
-            <div className="col-4 col-sm-12 p-4 mx-4 text-white">
+            <div className="col col-sm-4 p-4 mx-4 text-white">
                 <i className="my-auto icon-skill fa-brands fa-bootstrap"></i>
                 <p className="my-auto p-skill px-3 card-text">Boostrap</p>
             </div>
-            <div className="col-4 col-sm-12 p-4 mx-4 text-white">
+            <div className="col col-sm-4 p-4 mx-4 text-white">
                 <i className="fa-brands icon-skill fa-react"></i>
                 <p className="my-auto p-skill px-3 card-text">React</p>
             </div>
-            <div className="col-4 col-sm-12 p-4 mx-4 text-white">
+            <div className="col col-sm-4 p-4 mx-4 text-white">
                 <i className="fa-brands icon-skill fa-github"></i>
                 <p className="my-auto p-skill px-3 card-text">GitHub</p>
             </div>
-            <div className="col-4 col-sm-12 p-4 mx-4 text-white">
+            <div className="col col-sm-4 p-4 mx-4 text-white">
                 <i className="fa-solid icon-skill fa-flask"></i>
                 <p className="my-auto p-skill px-3 card-text">Flask</p>
             </div>
-            <div className="col-4 col-sm-12 p-4 mx-4 text-white">
+            <div className="col col-sm-4 p-4 mx-4 text-white">
                 <i className="fa-solid icon-skill fa-database"></i>
                 <p className="my-auto p-skill px-3 card-text">MSql</p>
             </div>
