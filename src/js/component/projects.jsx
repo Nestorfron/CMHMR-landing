5e6bd73371d35517d87c2ebf36e94ea5c2c43e91
Nestor Frones <115091611+Nestorfron@users.noreply.github.com>
2024-07-04 10:11:17 +0000

    fix promlem 2

--- src/js/component/projects.jsx
+++ src/js/component/projects.jsx
@@ -3,14 +3,7 @@ import React from "react";
 
 export const Projects = () => (
     <div id="projects" className="fondo text-center pb-3">
-        <div class="card">
-            <img src="..." class="card-img-top" alt="..." />
-            <div class="card-body">
-                <h5 class="card-title">Card title</h5>
-                <p class="card-text">Some quick example</p>
-                <a href="#" class="btn btn-primary">Go somewhere</a>
-            </div>
-        </div>
+
     </div>
 
 );
