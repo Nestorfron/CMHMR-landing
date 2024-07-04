e99e55cabf2a53e7621ff642c5047b4d50ffa402
nestorfron <nestorfrones07@hotmail.com>
2024-07-03 23:01:09 -0300

    text agout

--- src/js/component/about.jsx
+++ src/js/component/about.jsx
@@ -11,7 +11,7 @@ export const About = () => (
     <div id="about" className="text-center">
         <div className="card-header container col-sm-12">
             <img src={img} alt="Logo" width="70%" height="50%" className="image d-inline-block align-text-top" />
-            <p className="text-white fs-5">Desarrollador Full Stack con experiencia en el desarrollo de aplicaciones web y móviles, especializado en tecnologías front-end y back-end. Apasionado por crear soluciones eficientes, seguras y escalables, con un enfoque en la experiencia del usuario y la optimización del rendimiento.</p>
+            <p className="text-white">Desarrollador Full Stack con experiencia en el desarrollo de aplicaciones web y móviles, especializado en tecnologías front-end y back-end. Apasionado por crear soluciones eficientes, seguras y escalables, con un enfoque en la experiencia del usuario y la optimización del rendimiento.</p>
         </div>
         <div className="card-body fs-2">
             <Link>
