90534b153a3be4fa87f3de4ee570fd7bf5a5112e
Nestor Frones <115091611+Nestorfron@users.noreply.github.com>
2024-07-04 14:43:17 +0000

    color

--- src/js/component/projects.jsx
+++ src/js/component/projects.jsx
@@ -14,8 +14,8 @@ export const Projects = () => (
                 <div className="card-body text-white">
                     <h5 className="card-title">Tienda Virtual</h5>
                     <p className="card-text">HTML-CSS-React</p>
-                    <a href="#" className="btn btn-primary m-1">Demo</a>
-                    <a href="#" className="btn btn-primary m-1">Code</a>
+                    <a href="#" className="project-boton btn btn-primary m-1">Demo</a>
+                    <a href="#" className="project-boton btn btn-primary m-1">Code</a>
                 </div>
             </div>
             <div className="project-card card col-8 col-md-4 m-4 rounded-4">
@@ -23,8 +23,8 @@ export const Projects = () => (
                 <div className="card-body text-white">
                     <h5 className="card-title">Tienda Virtual</h5>
                     <p className="card-text">HTML-CSS-React</p>
-                    <a href="#" className="btn btn-primary m-1">Demo</a>
-                    <a href="#" className="btn btn-primary m-1">Code</a>
+                    <a href="#" className="project-boton btn btn-primary m-1">Demo</a>
+                    <a href="#" className="project-boton btn btn-primary m-1">Code</a>
                 </div>
             </div>
             <div className="project-card card col-8 col-md-4 m-4 rounded-4">
@@ -32,8 +32,8 @@ export const Projects = () => (
                 <div className="card-body text-white">
                     <h5 className="card-title">Tienda Virtual</h5>
                     <p className="card-text">HTML-CSS-React</p>
-                    <a href="#" className="btn btn-primary m-1">Demo</a>
-                    <a href="#" className="btn btn-primary m-1">Code</a>
+                    <a href="#" className="project-boton btn btn-primary m-1">Demo</a>
+                    <a href="#" className="project-boton btn btn-primary m-1">Code</a>
                 </div>
             </div>
             <div className="project-card card col-8 col-md-4 m-4 rounded-4">
@@ -41,8 +41,8 @@ export const Projects = () => (
                 <div className="card-body text-white">
                     <h5 className="card-title">Tienda Virtual</h5>
                     <p className="card-text">HTML-CSS-React</p>
-                    <a href="#" className="btn btn-primary m-1">Demo</a>
-                    <a href="#" className="btn btn-primary m-1">Code</a>
+                    <a href="#" className="project-boton btn btn-primary m-1">Demo</a>
+                    <a href="#" className="project-boton btn btn-primary m-1">Code</a>
                 </div>
             </div>
             <div className="project-card card col-8 col-md-4 m-4 rounded-4">
@@ -50,8 +50,8 @@ export const Projects = () => (
                 <div className="card-body text-white">
                     <h5 className="card-title">Tienda Virtual</h5>
                     <p className="card-text">HTML-CSS-React</p>
-                    <a href="#" className="btn btn-primary m-1">Demo</a>
-                    <a href="#" className="btn btn-primary m-1">Code</a>
+                    <a href="#" className="project-boton btn btn-primary m-1">Demo</a>
+                    <a href="#" className="project-boton btn btn-primary m-1">Code</a>
                 </div>
             </div>
             <div className="project-card card col-8 col-md-4 m-4 rounded-4">
@@ -59,8 +59,8 @@ export const Projects = () => (
                 <div className="card-body text-white">
                     <h5 className="card-title">Tienda Virtual</h5>
                     <p className="card-text">HTML-CSS-React</p>
-                    <a href="#" className="btn btn-primary m-1">Demo</a>
-                    <a href="#" className="btn btn-primary m-1">Code</a>
+                    <a href="#" className="project-boton btn btn-primary m-1">Demo</a>
+                    <a href="#" className="project-boton btn btn-primary m-1">Code</a>
                 </div>
             </div>
         </div>
